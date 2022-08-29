f5c22a231b248f62bda3e87d93979a3a0da6ba3b
karetskiiVO <kar.vladi@yandex.ru>
2022-08-29 11:28:34 +0300

    Update strlib.cpp

--- strlib.cpp
+++ strlib.cpp
@@ -2,6 +2,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <assert.h>
 
 #define NULLBYTE '\0'
 #define EOF -1
@@ -20,21 +23,21 @@ int strLen (const String str) {
     return str_buf - str;
 }
 
-String strCpy (String origin, const String addition) {
-    int alen = strLen(addition);
+String strCpy (String s1, const String s2) {
+    int s2len = strLen(s2);
 
-    for (int i = 0; i < alen; i++) {
-        origin[i] = addition[i];
+    for (int i = 0; i < s2len; i++) {
+        s1[i] = s2[i];
     }
 
-    return origin;
+    return s1;
 }
 
-String strChr (const String str, int ch) {
+char* strChr (const String str, int ch) {
     char* ptr = NULL;
     int strl = strLen(str);
     
-    for (int i = 0; i < strl && ptr == NULL) {
+    for (int i = 0; i < strl && ptr == NULL; i++) {
         if (str[i] == (char)ch) {
             ptr = str + i;
         }
@@ -43,13 +46,56 @@ String strChr (const String str, int ch) {
     return ptr;
 }
 
-String strrChr (const String str, int ch) {
+char* strrChr (const String str, int ch) {
+    char* ptr = NULL;
+    int strl = strLen(str) - 1;
     
+    for (int i = strl; i > -1 && ptr == NULL; i--) {
+        if (str[i] == (char)ch) {
+            ptr = str + i;
+        }
+    }
+    
+    return ptr;
 }
 
+int strCmp (const String s1, const String s2) {
+    int l1 = strLen(s1);
+    int l2 = strLen(s2);
 
+    int l = min(l1, l2);
 
+    for (int i = 0; i < l; i++) {
+        if (s1[i] < s2[i]) {
+            return -1;
+        }
+        if (s1[i] > s2[i]) {
+            return 1;
+        }
+    }
 
+    if (l1 < l2) {
+        return -1;
+    }
+    if (l1 > l2) {
+        return 1;
+    }
+
+    return 0;
+}
+
+String strCat (String s1, const String s2) {
+    assert(s1 != NULL && s2 != NULL && "the string mustnt be NULL\n");
+
+    int s1len = strLen(s1);
+    int s2len = strLen(s2);
+    
+    for(int i = 0; i < s2len; i++){
+        s1[i + s1len] = s2[i];
+    }
+
+    return 
+}
 
 #undef EOF
 #undef NULLBYTE
